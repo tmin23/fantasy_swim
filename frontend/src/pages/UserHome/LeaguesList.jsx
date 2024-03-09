f02eda8cb454ad11cd3f92ae0c4a5a69a240a195
Mike Dowd <mdowd4848@gmail.com>
2024-03-09 18:53:44 -0500

    idk

--- frontend/src/pages/UserHome/LeaguesList.jsx
+++ frontend/src/pages/UserHome/LeaguesList.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
-import { MDBContainer,
-MDBBtn,
-MDBRow,
-MDBCol,
-MDBCard,
-MDBCardBody,
-MDBCardTitle,
-MDBCardText,
-} from "mdb-react-ui-kit";
+import { styled } from '@mui/material/styles';
+import {Grid, Paper, Box, ButtonBase} from '@mui/material'
 
-import Button from '@mui/material/Button'
+const Item = styled(Paper)({
+    textAlign: 'center',
+  });
+
+const CustomButtonBase = styled(ButtonBase)({
+    width: '100%',
+    height: '300%',
+    borderRadius: '4px', // Add borderRadius to match the Paper component
+});
 
 export default function LeaguesList() {
     return (
-        <MDBContainer style={{marginTop: "10px", marginLeft:"0px"}} id="leagues">
-            <MDBRow className='mt-100'>
-                <MDBCol col='6'>
-                    league 1
-                </MDBCol>
-                <MDBCol col='6'>
-                    league 2
-                </MDBCol>
-            </MDBRow>
-            <MDBRow className='mt-100'>
-                <MDBCol col='6'>
-                    league 3
-                </MDBCol>
-                <MDBCol col='6'>
-                    league 4
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+        <Box sx={{ flexGrow: 1, marginTop: "50px", marginLeft: "20px", marginRight: "20px" }}>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <CustomButtonBase component={Item}>
+                        League 1
+                    </CustomButtonBase>
+                </Grid>
+                <Grid item xs>
+                    <CustomButtonBase component={Item}>
+                        League 2
+                    </CustomButtonBase>
+                </Grid>
+                <Grid item xs>
+                    <CustomButtonBase component={Item}>
+                        League 3
+                    </CustomButtonBase>
+                </Grid>
+            </Grid>
+            </Box>
     );
 }
