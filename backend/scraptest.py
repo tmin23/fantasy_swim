23bfae88093e0ec8af343d81dbcc0f229444471b
Tony Min <mint@rpi.edu>
2024-04-12 19:02:33 -0400

    Scrapper Test
    Zip file is for updated version of swimscrapper

---
+++ backend/scraptest.py
@@ -0,0 +1,48 @@
+from SwimScraper import SwimScraper as ss
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+meetlink = "https://www.swimcloud.com/results/296313/"
+# meetlink = "https://www.swimcloud.com/results/293735/"
+
+# Function to get the teams at the meetlink URL
+def getMeetTeams(meetlink):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options = chrome_options)
+    ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
+
+    driver.get(meetlink)
+    html = driver.page_source
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tag = soup.find_all('a', class_ = 'u-text-semi u-nowrap')
+    team_ids = [a['href'].split('/')[-2] for a in tag]
+    team_names = [a.get_text(strip=True) for a in tag]
+
+    team_dict = {team_names[i]: team_ids[i] for i in range(len(team_names))}
+    return team_dict
+
+# Function to get all swimmers on the teams
+def getMeetRosters(team_dict):
+    for team in team_dict:
+        TEAMID = team_dict[team]
+        team_roster = ss.getRoster(team = team, gender= "M", team_ID= TEAMID, year = 2023)
+        print(team)
+        for i in team_roster:
+            print(i)
+
+allTeams = getMeetTeams(meetlink)
+getMeetRosters(allTeams)
